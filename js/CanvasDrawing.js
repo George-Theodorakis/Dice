70363467f1cd74370a7705b8965c4eab3379b074
George-Theodorakis <gctheo@yahoo.com>
2014-11-24 19:59:02 -0500

    Add drawing code

--- js/CanvasDrawing.js
+++ js/CanvasDrawing.js
@@ -1,3 +1,25 @@
 var drawOnCanvas = function(canvas, probModel){
+	var fillColor = "#44aa22";
+	var outlineColor = "#225511";
 	
+	var context = canvas.getContext("2d");
+	context.fillStyle=fillColor;
+	context.strokeStyle=outlineColor;
+	var width = canvas.width;
+	var height = canvas.height;
+	var uniques = probModel.uniques;
+	var maxSize = probModel.maxValue;
+	var maxValue = probModel.array.length;
+	probModel.array.forEach(function(size,value){
+		var topy = height*size/maxSize;
+		var leftx = width*value/maxValue;
+		var rightx = width*(value+1)/maxValue;
+		context.rect(leftx,topy,rightx-leftx,height-topy);
+		context.fill();
+		context.stroke();
+	});
+}
+var drawCanvases = function(){
+	drawOnCanvas(document.getElementById("canvas1"),dice[0].probModel);
+	drawOnCanvas(document.getElementById("canvas2"),dice[1].probModel);
 }
