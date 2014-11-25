c37742442cd703f9666cde08c50258061da47a77
George-Theodorakis <gctheo@yahoo.com>
2014-11-25 18:30:02 -0500

    Added stdev/quartiles to probabilityModel

--- js/ProbabilityModel.js
+++ js/ProbabilityModel.js
@@ -7,6 +7,7 @@
 		this.cachedCdfFrac = {};
 		this.maxValue=0;
 		this.sum=0;
+		this.calculated=false;
 	}
 	ProbabilityModel.prototype.add = function(value,quantity){
 		this.cachedCdf = {};
@@ -18,14 +19,75 @@
 		this.count+=quantity;
 		this.sum+=value*quantity;
 		this.array[value]+=quantity;
-		this.stdev=undefined;
+		this.calculate=false;
 		this.maxValue = Math.max(this.maxValue,this.array[value]);
 	}
-	ProbabilityModel.prototype.standardDeviation = function(){
-		if(stdev===undefined){
-			//finish later
+	ProbabilityModel.prototype.calculate = function(){
+		var mean = this.sum/this.count;
+		var count=0;
+		var variance=0;
+		var quartilesLow = [0];
+		var quartilesHigh = [0];
+		this.array.forEach(function(quantity,value){
+			count+=quantity;
+			variance+=quantity*(value-mean)*(value-mean);
+			if(!quartilesLow[1]&&count>=quantity/4){
+				quartilesLow[1]=value;
+			}		
+			if(!quartilesHigh[1]&&count>quantity/4){
+				quartilesHigh[1]=value;
+			}
+			if(!quartilesLow[2]&&count>=quantity/2){
+				quartilesLow[2]=value;
+			}
+			if(!quartilesHigh[2]&&count>quantity/2){
+				quartilesHigh[2]=value;
+			}
+			if(!quartilesLow[3]&&count>=3*quantity/4){
+				quartilesLow[3]=value;
+			}
+			if(!quartilesHigh[3]&&count>3*quantity/4){
+				quartilesHigh[3]=value;
+			}
+		});
+		this.variance=variance;
+		this.stdev = Math.sqrt(variance);
+		var quartiles = [];
+		quartiles[0]=0;//todo make this min
+		quartiles[2]=(quartilesLow[2]+quartilesHigh[2])/2;
+		var lowWeight,heightWeight;//for first quartile, flipped for third
+		if(this.count%2==0){
+			lowWeight=.5;
+			highWeight=.5;
+		}else if((this.count+1)%4==0){
+			lowWeight=.25;
+			highWeight=.75;
+		}else{
+			lowWeight=.75;
+			highWeight=.25;
 		}
-		return stdev;
+		quartiles[1]=quartilesLow[1]*lowWeight+quartilesHigh[1]*highWeight;
+		quartiles[3] = quartilesLow[3]*highWeight+quartilesHigh[3]*lowWeight;
+		quartiles[4] = this.maxValue;
+		this.quartiles=quartiles;
+	}
+	
+	ProbabilityModel.prototype.mean = function(){
+		return this.sum/this.count;
+	}
+	ProbabilityModel.prototype.standardDeviation = function(){
+		if(!this.calculated){
+			this.calculate();
+			this.calculated=true;
+		}
+		return this.stdev;
+	}
+	ProbabilityModel.prototype.quartiles = function(){
+		if(!this.calculated){
+			this.calculate();
+			this.calculated=true;
+		}
+		return this.quartiles;
 	}
 	ProbabilityModel.prototype.cdfTotal = function(value){
 		if(this.cachedCdf[value]===undefined){
